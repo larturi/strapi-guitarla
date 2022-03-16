0022a22397e4a1454b698c7fe95cb5239791ef78
Leandro Arturi <lea.arturi@gmail.com>
2022-03-16 19:34:14 -0300

    Config

--- config/database.js
+++ config/database.js
@@ -1,20 +1,20 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'mongoose',
-      settings: {
-        host: env('DATABASE_HOST', 'cluster0.x708j.mongodb.net'),
-        srv: env.bool('DATABASE_SRV', true),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'strapi-guitarla'),
-        username: env('DATABASE_USERNAME', 'guitarladb'),
-        password: env('DATABASE_PASSWORD', 'guitarladb2022!'),
-      },
-      options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', 'admin'),
-        ssl: env.bool('DATABASE_SSL', true),
-      },
+    defaultConnection: 'default',
+    connections: {
+        default: {
+            connector: 'mongoose',
+            settings: {
+                host: env('DATABASE_HOST'),
+                srv: env.bool('DATABASE_SRV'),
+                port: env.int('DATABASE_PORT'),
+                database: env('DATABASE_NAME'),
+                username: env('DATABASE_USERNAME'),
+                password: env('DATABASE_PASSWORD'),
+            },
+            options: {
+                authenticationDatabase: env('AUTHENTICATION_DATABASE', 'admin'),
+                ssl: env.bool('DATABASE_SSL', true),
+            },
+        },
     },
-  },
 });
